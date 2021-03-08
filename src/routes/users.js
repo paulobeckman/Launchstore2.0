4380afd0def6dd420cf55e308a21fce5e088cf5d
paulobeckman <beckmam.paulo@hotmail.com>
2021-03-08 18:29:08 -0300

    Login logou

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,13 +4,14 @@ const routes = express.Router()
 const SessionController = require('../app/controllers/SessionController')
 const UserController = require('../app/controllers/UserController')
 
-const validator = require('../app/validators/user')
+const UserValidator = require('../app/validators/user')
+const SessionValidator = require('../app/validators/session')
 
 
 // // login/logout
-// routes.get('/login', SessionController.loginForm)
-// routes.post('/login', SessionController.login)
-// routes.post('/logout', SessionController.logout)
+routes.get('/login', SessionController.loginForm)
+routes.post('/login',SessionValidator.login, SessionController.login)
+routes.post('/logout', SessionController.logout)
 
 // //reset password / forgot
 // routes.get('/forgot-password', SessionController.forgotForm)
@@ -20,10 +21,10 @@ const validator = require('../app/validators/user')
 
 // //user register UserController
 routes.get('/register', UserController.registerForm)
-routes.post('/register',validator.post, UserController.post)
+routes.post('/register', UserValidator.post, UserController.post)
 
-routes.get('/', UserController.show)
-// routes.put('/', UserController.update)
+routes.get('/', UserValidator.show, UserController.show)
+routes.put('/', UserValidator.update, UserController.update)
 // routes.delete('/', UserController.delete)
 
 module.exports = routes
